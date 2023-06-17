bfa4b8ddad0500f53926cd685aefe6f8c4712154
NimeshaMG <mrnimesha74@gmail.com>
2023-06-17 14:31:12 -0400

    Updated configuration and package files

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const config = require('../config');
+
 module.exports = function() {
-const db = mongoose.connect(config.db);
-require('../app/models/user.server.model');
-return db;
-}
+  const db = mongoose.connect(config.db);
+  require('../app/models/user.server.model');
+  return db;
+};
